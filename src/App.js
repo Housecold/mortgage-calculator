9fd850d0a30a767be2fae507e1e506f5eca1e4e9
Housecold <danieledgarmagalhaes@gmail.com>
2024-10-10 15:50:46 +0100

    mortgage-calculator project done

--- src/App.js
+++ src/App.js
@@ -6,23 +6,25 @@ import Result from "./components/Result";
 import { Container, Grid2 } from "@mui/material";
 
 function App() {
-  
-  const [a, setA] = useState(15);
-
-  setA(30);
-
+  const [data, setData] = useState({
+    homeValue: 3000,
+    downPayment: 3000 * 0.2,
+    loanAmount: 3000 * 0.8,
+    loanTerm: 5,
+    interestRate: 5,
+  });
 
   return (
     <div className="App">
       <Navbar />
       <Container maxWidth="lg" sx={{ marginTop: 4 }}>
-        <Grid2 container spacing={5}>
+        <Grid2 container spacing={5} alignItems="center">
           <Grid2 item size={{ xs: 12, md: 6 }}>
-            <SliderSelect />
-            <TenureSelect />
+            <SliderSelect data={data} setData={setData} />
+            <TenureSelect data={data} setData={setData} />
           </Grid2>
           <Grid2 item size={{ xs: 12, md: 6 }}>
-            <Result />
+            <Result data={data} />
           </Grid2>
         </Grid2>
       </Container>
